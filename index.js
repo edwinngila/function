60494d573056a9fee8f09638812484b7f268bbe8
Edwin ngila kyalo <edwinngilakyalo@gmail.com>
2024-06-29 15:57:14 +0300

    update

--- index.js
+++ index.js
@@ -5,36 +5,35 @@ const app = express();
 const functions = require('firebase-functions');
 const app = require('./server');
 
-exports.sendEmail = functions.https.onRequest(async (request, response) => {
-  try {
-    // Create a transporter with your email provider's configuration
-    const {securityCode,UserEmail}=req.body;
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      auth: {
-        user: 'stockpilotorg@gmail.com',
-        pass: 'pyxh jhtc eqfl autp',
-      },
-    });
-
-    // Define the email options
-    const mailOptions = {
-      from: 'stockpilotorg@gmail.com',
-      to: `${UserEmail}`,
-      subject: 'Test Email from Firebase',
-      text: `${securityCode}`,
-    };
-
-    // Send the email
-    await transporter.sendMail(mailOptions);
-
-    response.send('Email sent successfully!');
-  } catch (error) {
-    console.error('Error sending email:', error);
-    response.status(500).send('Error sending email');
-  }
-});
+app.get('sendEmail',async (request,response)=>{
+    try {
+        const {securityCode,UserEmail}=request.body;
+        const transporter = nodemailer.createTransport({
+            host: "smtp.gmail.com",
+            port: 587,
+            auth: {
+              user: 'stockpilotorg@gmail.com',
+              pass: 'pyxh jhtc eqfl autp',
+            },
+          });
+      
+          // Define the email options
+          const mailOptions = {
+            from: 'stockpilotorg@gmail.com',
+            to: `${UserEmail}`,
+            subject: 'Test Email from Firebase',
+            text: `${securityCode}`,
+          };
+      
+          // Send the email
+          await transporter.sendMail(mailOptions);
+      
+          response.send('Email sent successfully!');
+    } catch (error) {
+        console.error('Error sending email:', error);
+        response.status(500).send('Error sending email');
+    }
+})
 app.use((req,res,next)=>{
     const err = new Error("NOT FOUND");
     err.status=404;
@@ -52,4 +51,4 @@ app.use((err,req,res,next)=>{
  // Adjust the path as necessary
 
 exports.app = functions.https.onRequest(app);
-app.listen(4000,()=>{console.log(`you are now listening to http://localhost:${process.env.PORT}`)})
+app.listen(4000,()=>{console.log(`you are now listening to http://localhost:4000`)})
